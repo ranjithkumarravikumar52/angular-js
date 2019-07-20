8802b935e775767f5ec88a238d6d6b7a87eab6aa
ranjith kumar <ranjithkumar@iesoftek.com>
2019-07-20 16:12:47 +0530

    * updated data phone list data on phone-list.component * added ng-model for search query. Closes #16. * added ng-model for sort by * added query in index to test scope of controller objects. Closes #17.

--- phone-cdo/phone-list/phone-list.component.js
+++ phone-cdo/phone-list/phone-list.component.js
@@ -4,13 +4,19 @@ angular.module('phoneList')
     controller : function phoneListController(){
         this.phones = [
             {
-                name : 'iphone 5',
-                price : 500
-            },
-            {
-                name : 'nexus',
-                price : 150
+                name: 'Nexus S',
+                snippet: 'Fast just got faster with Nexus S.',
+                age: 1
+            }, {
+                name: 'Motorola XOOM™ with Wi-Fi',
+                snippet: 'The Next, Next Generation tablet.',
+                age: 2
+            }, {
+                name: 'MOTOROLA XOOM™',
+                snippet: 'The Next, Next Generation tablet.',
+                age: 3
             }
         ];
+        this.orderProp = 'age';
 }
 });
